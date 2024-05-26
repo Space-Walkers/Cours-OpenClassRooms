9300b25fa4a2b59b74300b2dc9e6cd9a3a9f27d7
Space-Walkers <romain.royer1234@gmail.com>
2024-05-26 19:04:32 +0200

    import des new fichier

---
+++ dico.py
@@ -0,0 +1,12 @@
+
+dico = {
+"cle1" : "je suis genial", 
+
+"cle2": "j'ai une grosse bite", 
+
+"cle3" : "je suis malheureux",
+
+"test" : ["samira", "draven"]
+}
+
+print(dico["cle2"])
--- liste.py
+++ liste.py
@@ -1,1 +1,55 @@
-plateformes_sociales = ["Facebook", "Instagram", "Snapchat", "Twitter"]
+
+
+fruits=["pomme", "banane", "orange"]
+
+fruits.append("kiwi")
+print(fruits)
+
+fruits.remove("orange")
+print(fruits)
+
+fruits[1]=("ananas")
+print(fruits)
+
+print(len(fruits))
+
+fruits.sort()
+print(fruits)
+
+r=("romauin", "mflfl")
+print(r)
+
+
+
+'''
+pla = ["Facebook", "Instagram", "Snapchat", "Twitter"]
+print (pla)
+nom = "Romain"
+print (nom[1])
+
+pla.append("TikTok")
+print (pla)
+
+pla.remove(("Snapchat"))
+print (pla)
+
+len(pla)
+
+pla.sort()
+'''
+
+'''
+extend() Ajoute plusieurs éléments à la fin de la liste.
+
+insert() Insère un élément à une position donnée dans la liste.
+
+pop() Supprime et renvoie l'élément à une position donnée de la liste, ou le dernier élément si aucun indice n'est spécifié.
+
+index() Renvoie la première occurrence de l'élément spécifié dans la liste.
+
+count() Renvoie le nombre d'occurrences de l'élément spécifié dans la liste.
+
+reverse() Inverse l'ordre des éléments de la liste.
+
+'''
+
